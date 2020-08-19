cae76709bcb6867c499589b06a5ed0c8571ee188
sebas <sebasdripa07@gmail.com>
2020-08-19 00:11:56 -0500

    Signed-off-by: sebas <sebasdripa07@gmail.com>

--- comedordb(simp).sql
+++ comedordb(simp).sql
@@ -27,16 +27,6 @@ DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `comedordb`.`ciclo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS ciclo (
-  codC INT NOT NULL,
-  PRIMARY KEY (codC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
 -- Table `comedordb`.`comidas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS comidas (
@@ -57,16 +47,12 @@ CREATE TABLE IF NOT EXISTS estudiante (
   apelE VARCHAR(45) NOT NULL,
   facultad_codF INT NOT NULL,
   carrera_codCar INT NOT NULL,
-  ciclo_codC INT NOT NULL,
   telfE VARCHAR(9) NULL DEFAULT NULL,
   dirE VARCHAR(200) NULL DEFAULT NULL,
   PRIMARY KEY (codE),
   CONSTRAINT fk_estudiante_carrera1
     FOREIGN KEY (carrera_codCar)
     REFERENCES carrera (codCar),
-  CONSTRAINT fk_estudiante_ciclo1
-    FOREIGN KEY (ciclo_codC)
-    REFERENCES ciclo (codC),
   CONSTRAINT fk_estudiante_facultad1
     FOREIGN KEY (facultad_codF)
     REFERENCES facultad (codF))
@@ -89,14 +75,13 @@ DEFAULT CHARACTER SET = utf8;
 -- Table `comedordb`.`usuario`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS usuario (
-  codU INT NOT NULL,
-  passU VARCHAR(200) NOT NULL,
   dniU CHAR(8) NOT NULL,
+  passU VARCHAR(200) NOT NULL,
   nombU VARCHAR(45) NOT NULL,
   apelU VARCHAR(45) NOT NULL,
   telfU VARCHAR(9) NULL DEFAULT NULL,
   dirU VARCHAR(200) NULL DEFAULT NULL,
-  PRIMARY KEY (codU))
+  PRIMARY KEY (dniU))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -105,12 +90,12 @@ DEFAULT CHARACTER SET = utf8;
 -- Table `comedordb`.`tarjeta`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS tarjeta (
-  codT CHAR(6) NOT NULL,
+  codT INT NOT NULL,
   sem CHAR(5) NOT NULL,
   fechCreacion DATE NOT NULL,
   estudiante_codE CHAR(10) NOT NULL,
   sede_codS INT NOT NULL,
-  usuario_codU INT NOT NULL,
+  usuario_dniU char(8) NOT NULL,
   PRIMARY KEY (codT, sem),
   CONSTRAINT fk_tarjeta_estudiante1
     FOREIGN KEY (estudiante_codE)
@@ -119,8 +104,8 @@ CREATE TABLE IF NOT EXISTS tarjeta (
     FOREIGN KEY (sede_codS)
     REFERENCES sede (codS),
   CONSTRAINT fk_tarjeta_usuario1
-    FOREIGN KEY (usuario_codU)
-    REFERENCES comedordb.usuario (codU))
+    FOREIGN KEY (usuario_dniU)
+    REFERENCES comedordb.usuario (dniU))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -129,12 +114,12 @@ DEFAULT CHARACTER SET = utf8;
 -- Table `comedordb`.`registro`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS registro (
-  tarjeta_codT CHAR(6) NOT NULL,
+  tarjeta_codT INT NOT NULL,
   tarjeta_sem CHAR(5) NOT NULL,
   comidas_codCo INT NOT NULL,
   fecha DATE NOT NULL,
   hora TIME NOT NULL,
-  usuario_codU INT NOT NULL,
+  usuario_dniU char(8) NOT NULL,
   PRIMARY KEY (tarjeta_codT, tarjeta_sem, comidas_codCo, fecha),
   CONSTRAINT fk_tarjeta_has_comidas_comidas1
     FOREIGN KEY (comidas_codCo)
@@ -143,25 +128,13 @@ CREATE TABLE IF NOT EXISTS registro (
     FOREIGN KEY (tarjeta_codT , tarjeta_sem)
     REFERENCES tarjeta (codT , sem),
   CONSTRAINT fk_tarjeta_has_comidas_usuario1
-    FOREIGN KEY (usuario_codU)
-    REFERENCES usuario (codU))
+    FOREIGN KEY (usuario_dniU)
+    REFERENCES usuario (dniU))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 
 -- Registros
--- -Ciclo
-insert into ciclo values (1);
-insert into ciclo values (2);
-insert into ciclo values (3);
-insert into ciclo values (4);
-insert into ciclo values (5);
-insert into ciclo values (6);
-insert into ciclo values (7);
-insert into ciclo values (8);
-insert into ciclo values (9);
-insert into ciclo values (10);
-
 -- -Facultad
 insert into facultad values (1, "Ingenieria");
 insert into facultad values (2, "Ciencias de la empresa");
