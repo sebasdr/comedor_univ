95ba4206884c9085b35d9266ee7d55b88406a094
sebas <sebasdripa07@gmail.com>
2020-09-03 00:51:45 -0500

    Implementando tablas de registro de estudiante

--- php/funcionesbas.php
+++ php/funcionesbas.php
@@ -18,7 +18,8 @@ if ($conexion ->connect_error) die("No es posible conectar con la base de datos"
     echo json_encode("er1");
 } */
 
-if (isset($_POST["codigo"]) && isset($_POST["dni"]) && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["facultad"]) && isset($_POST["carrera"])){
+//Este es el bueno
+/* if (isset($_POST["codigo"]) && isset($_POST["dni"]) && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["facultad"]) && isset($_POST["carrera"])){
     reg_est($conexion);
 }elseif(isset($_POST["codigot"]) && isset($_POST["sem"]) && isset($_POST["fecha"]) && isset($_POST["codigoe"]) && isset($_POST["sede"])){
     asig_tar($conexion);
@@ -26,7 +27,9 @@ if (isset($_POST["codigo"]) && isset($_POST["dni"]) && isset($_POST["nombre"]) &
     sellar_tar($conexion);
 }else{
     echo json_encode("er1");
-}
+} */
+
+tab_reg_est($conexion);
 
 //Registrar estudiante
 function reg_est($conexion){
@@ -116,6 +119,35 @@ function reg_est($conexion){
         </script>
 _END;*/
     }
+}
+
+function tab_reg_est($conexion){
+    $query="SELECT * FROM estudiante ORDER BY codE";
+    $result=$conexion->query($query);
+    $a = array();//CReando array para guardar json
+
+    if($result->num_rows){
+        $rows = $result->num_rows;
+        for($j=0; $j<$rows; $j++){
+            $row=$result->fetch_array(MYSQLI_NUM);
+            $query2="SELECT c.nombCar, f.nombF FROM carrera c inner JOIN facultad f on c.facultad_codF = f.codF WHERE c.codCar='$row[5]'";
+            $result2=$conexion->query($query2);
+            $rows2=$result2->fetch_array(MYSQLI_NUM);
+            $codigo=htmlspecialchars($row[0]);
+            $dni=htmlspecialchars($row[1]);
+            $nombre=htmlspecialchars($row[2]);
+            $apellido=htmlspecialchars($row[3]);
+            $facultad=htmlspecialchars($rows2[1]);
+            $carrera=htmlspecialchars($rows2[0]);
+            $telefono=htmlspecialchars($row[6]);
+            $direccion=htmlspecialchars($row[7]);
+            $objeto = new tabregest ($codigo,$dni,$nombre,$apellido,$facultad,$carrera,$telefono,$direccion);//CReando objeto
+            $a[$j]=$objeto;//Guardando objeto en array
+            $result2->close();
+        }
+        echo json_encode($a);//Enviando json con info correctamente ordenada
+        $result->close();   
+    }else echo json_encode("erif");
 }
 
 //Asignar tarjeta
@@ -270,6 +302,29 @@ function mysql_entities_fix_string($conexion, $string){
 function mysql_fix_string($conexion, $string){
     if (get_magic_quotes_gpc()) $string = stripslashes($string);
     return $conexion->real_escape_string($string);
+}
+
+//Clases para crear objetos para json
+class tabregest{
+    var $codigo;
+    var $dni;
+    var $nombre;
+    var $apellido;
+    var $facultad;
+    var $carrera;
+    var $telefono;
+    var $direccion;
+
+    function __construct($codigo,$dni,$nombre,$apellido,$facultad,$carrera,$telefono,$direccion){
+        $this->codigo = $codigo;
+        $this->dni = $dni;
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->facultad = $facultad;
+        $this->carrera = $carrera;
+        $this->telefono = $telefono;
+        $this->direccion = $direccion;
+    }
 }
 
 $conexion->close();
